c2a57ea2066fb211e90c5909af7283b11176af63
Aloïs DELAUNE <alois.delaune@hotmail.fr>
2021-12-16 19:17:22 +0100

    improved the form using bootstrap classes

--- src/app/search-movie/search-movie.component.ts
+++ src/app/search-movie/search-movie.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+
 
 @Component({
   selector: 'app-search-movie',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-movie.component.scss']
 })
 export class SearchMovieComponent implements OnInit {
+  searchMovieForm = this.fb.group({
+    id: ['', Validators.required],
+    title: ['', Validators.required],
+    type: ['', Validators.required],
+    year: ['', Validators.required],
+    information: ['', Validators.required]
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.searchMovieForm.value);
   }
 
 }
